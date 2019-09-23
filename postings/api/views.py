0aa581db9b8391a10c2fda13c2caf6a2404ddb79
Igor Kwiatkowski <waffel@gmail.com>
2019-09-23 14:59:46 +0200

    added serializer

--- postings/api/views.py
+++ postings/api/views.py
@@ -1,9 +1,11 @@
 from rest_framework import generics
 from postings.models import BlogPost
+from .serializers import BlogPostSerializer
 
 
 class BlogPostRudView(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = 'pk'
+    serializer_class = BlogPostSerializer
 
     def get_queryset(self):
         return BlogPost.objects.all()
