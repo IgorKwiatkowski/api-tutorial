73514bcf03ec78fa98d7ac5908c87db4777570d7
Igor Kwiatkowski <waffel@gmail.com>
2019-09-24 11:10:45 +0200

    added unique title validator

--- postings/api/serializers.py
+++ postings/api/serializers.py
@@ -13,3 +13,10 @@ class BlogPostSerializer(serializers.ModelSerializer):
             'timestamp',
         ]
 
+    def validate_title(self, value):
+        qs = BlogPost.objects.filter(title__iexact=value)
+        if self.instance:
+            qs = qs.exclude(pk=self.instance.pk)
+        if qs.exists():
+            raise serializers.ValidationError('Title is not unique')
+        return value
