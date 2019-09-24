4c01493c53070fc295f70ac708de80e458ddcde9
Igor Kwiatkowski <waffel@gmail.com>
2019-09-24 12:10:35 +0200

    added search feature

--- postings/api/views.py
+++ postings/api/views.py
@@ -1,17 +1,28 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 from postings.models import BlogPost
 from .serializers import BlogPostSerializer
+from django.db.models import Q
 
 
-class BlogPostAPIView(generics.CreateAPIView):
+class BlogPostAPIView(mixins.CreateModelMixin, generics.ListAPIView):
     lookup_field = 'pk'
     serializer_class = BlogPostSerializer
 
     def get_queryset(self):
-        return BlogPost.objects.all()
+        qs = BlogPost.objects.all()
+        query = self.request.GET.get('q')
+        if query is not None:
+            qs = qs.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query)
+            ).distinct()
+        return qs
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
 class BlogPostRudView(generics.RetrieveUpdateDestroyAPIView):
